8415946a7cc07622d22e5ed5a6c5a8e5c0ec65b6
SGPD\dmuppaneni <divya.gorantla@safeguardproperties.com>
2017-02-16 17:21:51 -0600

    Adding one more test case

--- packages/concurrent/concurrent_test.go
+++ packages/concurrent/concurrent_test.go
@@ -1,6 +1,9 @@
 package concurrent
 
-import "testing"
+import (
+	"errors"
+	"testing"
+)
 
 var commandTests = []struct {
 	command string
@@ -10,12 +13,16 @@ var commandTests = []struct {
 		"C://work//src//github.com//divyag9//encryptbackup//encryptbackup -sd D://Users//dmuppaneni//Documents//source -td D://Users//dmuppaneni//Documents//target -sgpkey D://Users//dmuppaneni//Documents//pubring.gpg -midkey D://Users//dmuppaneni//Documents//pubring.gpg",
 		nil,
 	},
+	{
+		"C://work//src//github.com//divyag9//encryptbackup//encrypt -sd D://Users//dmuppaneni//Documents//source -td D://Users//dmuppaneni//Documents//target -sgpkey D://Users//dmuppaneni//Documents//pubring.gpg -midkey D://Users//dmuppaneni//Documents//pubring.gpg",
+		errors.New("exec: \"C://work//src//github.com//divyag9//encryptbackup//encrypt\": file does not exist"),
+	},
 }
 
 func TestExecuteCommand(t *testing.T) {
 	for _, commandTests := range commandTests {
 		_, err := ExecuteCommand(commandTests.command)
-		if err != commandTests.err {
+		if err != commandTests.err && err.Error() != commandTests.err.Error() {
 			t.Errorf("Returned: %v. Expected: %v", err, commandTests.err)
 		}
 	}
